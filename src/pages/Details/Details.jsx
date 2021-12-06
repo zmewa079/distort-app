d7f90b7e2ed3b8bd5150eced4fd45403886dc1dd
zmewa079 <zannatul.mewa07@gmail.com>
2021-12-06 17:09:51 -0500

    add embedded wiki page link

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -8,16 +8,15 @@ const suggestion = location.state
 
   return (
       <>
-          <main >
-            {suggestion?.Name}
-            {suggestion.Type}
-            {suggestion.wTeaser}
-            {suggestion.wUrl}
-            {suggestion.yUrl}
-          </main>
+        <main>
           <body>
-    
+          <p>Artist Name: {suggestion.Name}</p>
+            <p>Type: {suggestion.Type}</p>
+            <p>Description: {suggestion.wTeaser}</p>
+            <p>Wikipedia Description: <a href={suggestion.wUrl}>View wikipedia page</a></p>
+            <p>Youtube: <iframe width="420" height="315" src={suggestion.yUrl} /></p>
           </body>
+        </main>
       </>
   )
             }
