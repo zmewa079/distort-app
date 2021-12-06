5340fe8e106403951aabbfdd2c7d33a1848ebda7
RamonM47 <aviramon.mesa@gmail.com>
2021-12-06 11:00:36 -0500

    add follow button conditional to user cards

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,32 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import * as userService from '../../services/userService'
-import styles from './Users.css'
+import './Users.css'
 
-const Users = () => {
+const Users = (props) => {
   const [users, setUsers] = useState([])
 
-  useEffect(()=> {
+  useEffect(() => {
     userService.getAllUsers()
-    .then(users => setUsers(users))
+      .then(users => setUsers(users))
   }, [])
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    try {
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-     <div class= 'profilecard'>
-      <h1 class='hello'>Hello.  This is a list of all the users.</h1>
-      {users.length ? 
+    <div class='profilecard'>
+      <h1 class='hello' >Hello. This is a list of all the users.</h1>
+      {users.length ?
         <>
-          {users.map(user=>
-            <div class='pfcard'>
-              <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwwUXumi2-4L46JVm7bryAzGyWCD1SHYLfIw&usqp=CAU'></img>
-              <h2 class='name' key={user._id}>{user.name}</h2>
-              <h2 class='mvlst' >List of Movies</h2>
-              <h2 class='mclst'>List of Music</h2>
-              <h2 class='pdlst'>List of Podcasts</h2>
-              <h3 class='fdlst'>Followers (0)</h3>
+          {users.map(user =>
+              <div class='pfcard'>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwwUXumi2-4L46JVm7bryAzGyWCD1SHYLfIw&usqp=CAU'></img>
+                <h2 class='name' key={user._id}>{user.name}</h2>
+                <h2 class='mvlst' >List of Movies</h2>
+                <h2 class='mclst'>List of Music</h2>
+                <h2 class='pdlst'>List of Podcasts</h2>
+                <h3 class='fdlst'>Followers (0)</h3>
+                {props.loggedInUser.profile === user.profile ? '' :
+                  <form action="" onSubmit={handleSubmit} >
+                    <button>Follow</button>
+                  </form>
+                }
             </div>
           )}
-      </>
-      :
+        </>
+        :
         <p>An error occured</p>
       }
     </div>
