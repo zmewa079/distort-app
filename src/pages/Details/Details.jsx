90f3f8bac42bcb178cc4fe242f1afd0bbb2103e8
RamonM47 <aviramon.mesa@gmail.com>
2021-12-09 22:37:15 -0500

    style description page

--- src/pages/Details/Details.jsx
+++ src/pages/Details/Details.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom'
 import Music from '../Music/Music';
+import './Details.css'
 
 const Details = () => {
 const location = useLocation()
@@ -9,12 +10,21 @@ const suggestion = location.state
   return (
       <>
         <main>
-          <body>
+          <body className='contents'>
           <p>Artist Name: {suggestion.Name}</p>
-            <p>Type: {suggestion.Type}</p>
-            <p>Description: {suggestion.wTeaser}</p>
-            <p>Wikipedia Description: <a href={suggestion.wUrl}>View wikipedia page</a></p>
-            <p>Youtube: <iframe width="420" height="315" src={suggestion.yUrl} /></p>
+            <p className='type'>
+              Type: {suggestion.Type}
+            </p>
+            <p className='desc'>
+              Description: {suggestion.wTeaser}
+            </p>
+            <p className='wiki'>
+              Wikipedia Description: <a href={suggestion.wUrl}>View wikipedia page</a>
+            </p>
+            <div className='youtubec'>
+              <p>Youtube: </p>
+              <iframe width="420" height="315" src={suggestion.yUrl} />
+            </div>
           </body>
         </main>
       </>
